e142b6e267e3aa8cb937210e0be9cc97cd5a02a7
Fabrizio <fabriziodiblasi92@gmail.com>
2019-08-05 17:13:06 +0200

    esempio

---
+++ example/build/CMakeFiles/ipm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ipm.dir/main.cpp.o"
+  "CMakeFiles/ipm.dir/IPM.cpp.o"
+  "ipm.pdb"
+  "ipm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ipm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
