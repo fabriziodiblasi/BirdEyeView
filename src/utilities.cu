02bdddb9cf5f38149acabf9ac1ff3e40173446ab
fabrizio <fabriziodiblasi92@gmail.com>
2019-09-21 19:55:58 +0200

    conclusioni relazione aggiornate

--- src/utilities.cu
+++ src/utilities.cu
@@ -171,103 +171,54 @@ void matToArray(float *array, const cv::Mat &mat, int rig, int col){
 // --------------------------------------------------------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------------------------------------------------------
-
+Mat tranImg;
 Mat warpPerspectiveCPU(Mat A, Mat H){
     // allocate array of all locations
     int Numrows = A.rows;
     int Numcols = A.cols;
     int channels   = A.channels();
-    // cout << "rows " << Numrows << "col " << Numcols << "channels " << channels <<endl;
     int size = Numrows*Numcols;
     int MaxX,MaxY = -1000;
     int MinX,MinY =  1000;
     int *TransArry = (int *)malloc(sizeof(int)*size);
     int Idx;
-
     int homeX, homeY;
-
-    // int homeX=Idx % Numcols;
-    // int homeY=Idx / Numcols;
-    // cout << H << endl;
+    
+    A.copyTo(tranImg);
+    tranImg = tranImg - tranImg;
 
     for (Idx=0; Idx < size; ++Idx ){
 
         homeX=Idx % Numcols;
         homeY=Idx / Numcols;
-
-        // float x  = (H.at<float>(0,0) * (homeX)) +( H.at<float>(0,1) * (homeY)) + ( H.at<float>(0,2) * 1) ;
-        // float y  = (H.at<float>(1,0) * (homeX)) +( H.at<float>(1,1) * (homeY)) + ( H.at<float>(1,2) * 1) ;
-        // float s  = (H.at<float>(2,0) * (homeX)) +( H.at<float>(2,1) * (homeY)) + ( H.at<float>(2,2) * 1) ;
-
         float x  = (H.at<float>(0,0) * (homeX)) +( H.at<float>(0,1) * (homeY)) +  H.at<float>(0,2) ;
         float y  = (H.at<float>(1,0) * (homeX)) +( H.at<float>(1,1) * (homeY)) +  H.at<float>(1,2) ;
         float s  = (H.at<float>(2,0) * (homeX)) +( H.at<float>(2,1) * (homeY)) +  H.at<float>(2,2) ;
-
-
-        // cout << " x = " << x << " y= " << y << " s= " << s;
         x = floor(x/s);
-
         y = floor(y/s);
-
-        // for the first col in TransMatrix
         if (homeX ==0){
             if (x > MaxX) MaxX = x;
             if (x < MinX) MinX = x; 
         }
-
-        //for thee first row in TransMatrix
         if (homeY ==0){
             if (y > MaxY) MaxY = y;
             if (y < MinY) MinY = y;
         }
         if( y >= A.rows || y < 0 || x >= A.cols || x < 0){
             TransArry[Idx]  = -1;
-            // cout << "x= " << x << "y= "<< y << endl;
         }else{
-            TransArry[Idx] = (y * Numcols + x); 
-        }           
-
-        //cout << Numcols << endl;
-        // cout <<     "New index of " << Idx << "is " << TransArry[Idx] << endl;
-    }
-
-    Mat   tranImg ;
-
-    A.copyTo(tranImg);
-    tranImg = tranImg - tranImg;
-    
-    // Remap Image
-    for (Idx=0; Idx < size; Idx ++ ){
-
-        homeX=Idx % Numcols;
-        homeY=Idx / Numcols;                
-        //tranImg.at<uchar>(homeY, homeX) =0;
-        if(TransArry[Idx] != -1){   
-            //cout << "Index " << Idx << "Passed " << endl;
+            TransArry[Idx] = (y * Numcols + x);
+            homeX=Idx % Numcols;
+            homeY=Idx / Numcols; 
             int newhomeX=TransArry[Idx] % Numcols; // Col ID
             int newhomeY=TransArry[Idx] / Numcols;  // Row ID
-
-
-            // cout << "Index is " << Idx << endl;
-            // cout << "HomeX is " << homeX << " and HomeY is " << homeY << endl;
-            // cout << "New Index is " << TransArry[Idx] << endl;
-            // cout << "New HomeX is " << newhomeX << " and New HomeY is " << newhomeY << endl;   
-            // cout << "*****************************************"<< endl; 
-            // if (!(Idx%100)) sleep(20);  
-
             tranImg.at<uchar>(newhomeY, (newhomeX*channels)) = A.at<uchar>(homeY, homeX*channels);
             if(channels>1)
                 tranImg.at<uchar>(newhomeY, newhomeX*channels+1) = A.at<uchar>(homeY, homeX*channels+1);
             if(channels>2)
                 tranImg.at<uchar>(newhomeY, newhomeX*channels+2) = A.at<uchar>(homeY, homeX*channels+2);
-            // if (!(Idx%100)){
-                // imshow("inside", tranImg);
-                // waitKey(1);
-                // }
-            }
+        }           
     }
-    //cout << tranImg << endl;  
-    
     return tranImg;
 
 }
